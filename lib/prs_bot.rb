1814395cba734cce97479501e8b0992400ecfe8e
an-lee <an.lee.work@gmail.com>
2018-09-17 18:13:38 +0800

    basic design

---
+++ lib/prs_bot.rb
@@ -0,0 +1,53 @@
+require 'base64'
+require 'openssl'
+require_relative './prs_bot/client'
+
+module PrsBot
+  class<< self
+    attr_accessor :keystore, :password
+  end
+
+  def get_auth_header(options={})
+    #code
+  end
+
+  def sign_file(content, options={})
+    #code
+  end
+
+  def sign_file_viaKey(content, options={})
+    #code
+  end
+
+  def sign_image(file, options={})
+    #code
+  end
+
+  def sign_text(text, options={})
+    #code
+  end
+
+  def get_auth_signature(path, payload, options={})
+    #code
+  end
+
+  def roll_object(object)
+    #code
+  end
+
+  def get_pub_address_by_sig_and_msghash(sig, msghash, options={})
+    #code
+  end
+
+  def keccak256(message)
+    #code
+  end
+
+  def sha256(string)
+    #code
+  end
+
+  def create_key_pair(options={})
+    #code
+  end
+end
---
+++ prs_bot.gemspec
@@ -0,0 +1,16 @@
+$:.push File.expand_path("../lib", __FILE__)
+
+require "prs_bot/version"
+
+Gem::Specification.new do |s|
+  s.name          = %q{prs_bot}
+  s.version       = PrsBot::VERSION
+  s.authors       = ["an-lee"]
+  s.email         = ["an.lee.work@gmail.com"]
+  s.homepage      = "https://github.com/an-lee/prs_bot"
+  s.summary       = "An Unofficial API wrapper of PressOne"
+  s.description   = "An Unofficial API wrapper of PressOne"
+  s.license       = "MIT"
+  s.files         = Dir["{app,config,db,lib}/**/*", "MIT-LICENSE"]
+  s.require_paths = ["lib"]
+end
