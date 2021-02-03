477244a9f5feab22b0d982fd1e6a801975829d7d
an-lee <an.lee.work@gmail.com>
2021-02-03 22:22:40 +0800

    refactor: sign hash

--- lib/prs_bot/utility.rb
+++ lib/prs_bot/utility.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 require "eth"
+require "json"
+require "digest"
 
 module PrsBot
   # PRS crypto tools
@@ -27,18 +29,19 @@ module PrsBot
       key = Eth::Key.new(priv: private_key)
       hash_bin = Eth::Utils.hex_to_bin hash
       signature_bin = key.sign_hash hash_bin
-      v, r, s = Eth::Utils.v_r_s_for signature_bin
-      v -= 27
+      v = signature_bin[0].bytes[0]
+      v -= 27 if v > 27
       v = v.to_s(16)
       v = "0#{v}" if (v.size % 2).positive?
-      signature = r.to_s(16) + s.to_s(16) + v
-      [hash, signature]
+      Eth::Utils.bin_to_hex(signature_bin[1..65]) + v
     end
 
     def sig_to_address(hash, signature)
       hash_bin = Eth::Utils.hex_to_bin hash
       signature_bin = Eth::Utils.hex_to_bin(signature).bytes.rotate(-1).pack("c*")
       public_hex = Eth::OpenSsl.recover_compact hash_bin, signature_bin
+      raise "Failed to recover" unless public_hex
+
       address = Eth::Utils.public_key_to_address public_hex
       Eth::Utils.remove_hex_prefix(address).downcase
     end
@@ -47,9 +50,46 @@ module PrsBot
       Eth::Utils.bin_to_hex Eth::Utils.keccak256(message)
     end
 
+    def sha256(message)
+      Digest::SHA256.hexdigest message
+    end
+
     def sign_text(message, private_key, alg: "keccak256")
-      hash = keccak256 message
-      sign_hash hash, private_key
+      hash =
+        case alg
+        when "keccak256"
+          keccak256 message
+        when "sha256"
+          sha256 message
+        else
+          raise "#{alg} is not supported!"
+        end
+
+      [sign_hash(hash, private_key), hash]
+    end
+
+    def sign_block_data(data, private_key, alg: "keccak256")
+      raise "Not a hash data" unless data.is_a? Hash
+
+      hash =
+        case alg
+        when "keccak256"
+          keccak256 sort_hash(data)
+        when "sha256"
+          sha256 sort_hash(data)
+        else
+          raise "#{alg} is not supported!"
+        end
+
+      [sign_hash(hash, private_key), hash]
+    end
+
+    def sort_hash(hash)
+      keys = hash.keys.sort
+      _hash = {}
+      keys.each { |key| _hash[key] = hash[key] }
+
+      _hash.to_json
     end
   end
 end
--- test/prs_bot/utility_test.rb
+++ test/prs_bot/utility_test.rb
@@ -5,7 +5,7 @@ require "test_helper"
 module PrsBot
   class UtilityTest < Minitest::Test
     def test_recover_private_key
-      keystore = '{"address":"758ea2601697fbd3ba6eb6774ed70b6c4cdb0ef9","crypto":{"cipher":"aes-128-ctr","ciphertext":"92af6f6710eba271eae5ac7fec72c70d9f49215e7880a0c45d4c53e56bd7ea59","cipherparams":{"iv":"13ddf95d970e924c97e4dcd29ba96520"},"mac":"b9d81d78f067334ee922fb2863e32c14cbc46e479eeb0acc11fb31e39256004e","kdf":"pbkdf2","kdfparams":{"c":262144,"dklen":32,"prf":"hmac-sha256","salt":"79f90bb603491573e40a79fe356b88d0c7869852e43c2bbaabed44578a82bbfa"}},"id":"93028e51-a2a4-4514-bc1a-94b089445f35","version":3}'
+      keystore = File.read("./test/fixtures/keystore.json")
       password = "123123"
       private_key = PrsBot.utility.recover_private_key(keystore, password)
       assert_equal private_key, "6e204c62726a19fe3f43c4ca9739b7ffa37e4a3226f824f3e24e00a5890addc6"
@@ -33,7 +33,7 @@ module PrsBot
     def test_sign_hash
       hash = "a70b44e0a41bc225914180dc0785fd71f8f018d90e76e3c5687e027ad273b695"
       private_key = "6e204c62726a19fe3f43c4ca9739b7ffa37e4a3226f824f3e24e00a5890addc6"
-      hash, signature = PrsBot.utility.sign_hash hash, private_key
+      signature = PrsBot.utility.sign_hash hash, private_key
       addr_by_private_key = PrsBot.utility.private_key_to_address private_key
       addr_by_sig = PrsBot.utility.sig_to_address hash, signature
       assert_equal addr_by_private_key, addr_by_sig
@@ -49,10 +49,35 @@ module PrsBot
       assert_equal addr_by_private_key, addr_by_signature
     end
 
-    def test_sign_text
+    def test_sign_text_in_keccak256
       message = "hello"
       key_pair = PrsBot.utility.create_key_pair
-      hash, signature = PrsBot.utility.sign_text message, key_pair[:private_key]
+      alg = "keccak256"
+      signature, hash = PrsBot.utility.sign_text message, key_pair[:private_key], alg: alg
+      assert_equal PrsBot.utility.private_key_to_address(key_pair[:private_key]), PrsBot.utility.sig_to_address(hash, signature)
+    end
+
+    def test_sign_text_in_sha256
+      message = "hello"
+      key_pair = PrsBot.utility.create_key_pair
+      alg = "sha256"
+      signature, hash = PrsBot.utility.sign_text message, key_pair[:private_key], alg: alg
+      assert_equal PrsBot.utility.private_key_to_address(key_pair[:private_key]), PrsBot.utility.sig_to_address(hash, signature)
+    end
+
+    def test_sign_block_data_keccak256
+      data = { a: 111, b: 222 }
+      key_pair = PrsBot.utility.create_key_pair
+      alg = "keccak256"
+      signature, hash = PrsBot.utility.sign_block_data data, key_pair[:private_key], alg: alg
+      assert_equal PrsBot.utility.private_key_to_address(key_pair[:private_key]), PrsBot.utility.sig_to_address(hash, signature)
+    end
+
+    def test_sign_block_data_sha256
+      data = { a: 111, b: 222 }
+      key_pair = PrsBot.utility.create_key_pair
+      alg = "sha256"
+      signature, hash = PrsBot.utility.sign_block_data data, key_pair[:private_key], alg: alg
       assert_equal PrsBot.utility.private_key_to_address(key_pair[:private_key]), PrsBot.utility.sig_to_address(hash, signature)
     end
   end
